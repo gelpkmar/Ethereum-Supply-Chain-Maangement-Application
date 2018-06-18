3c76172b8923f3f4ca85894c555bc63b6a1f941c
Marlon Gelpke <marlon.gelpke@gmail.com>
2018-06-18 15:22:44 +0200

    cleanup commit

--- .next/dist/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-routes.add('/assets/new', '/assets/new').add('/assets/:address', '/assets/show');
-
-module.exports = routes;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJvdXRlcy5qcyJdLCJuYW1lcyI6WyJyb3V0ZXMiLCJyZXF1aXJlIiwiYWRkIiwibW9kdWxlIiwiZXhwb3J0cyJdLCJtYXBwaW5ncyI6Ijs7QUFBQSxJQUFNLFNBQVMsQUFBZjs7QUFFQSxPQUNHLEFBREgsSUFDTyxBQURQLGVBQ3NCLEFBRHRCLGVBRUcsQUFGSCxJQUVPLEFBRlAsb0JBRTJCLEFBRjNCOztBQUlBLE9BQU8sQUFBUCxVQUFpQixBQUFqQiIsImZpbGUiOiJyb3V0ZXMuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL01hcmxvbi9Qcm9qZWN0cy9ldGhlcmV1bV9wcm9qZWN0cy9TdXBwbHlDaGFpbk1hbmFnZW1lbnQifQ==
